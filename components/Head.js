d417f80c89abb2cba02250dc43577fea0169ab7c
winterandrea <andrecasero@gmail.com>
2021-08-07 11:39:49 +0200

    pwa

--- components/Head.js
+++ components/Head.js
@@ -4,9 +4,6 @@ import Head from 'next/head'
 const CustomHead = () => {
   return (
        <Head>
-        <meta charset='utf-8' />
-        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
         <meta name='description' content='Description' />
         <title>ELL-ET VETERINARIA</title>
       </Head>
